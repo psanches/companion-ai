a5a0bf5b9052c4fae38c0b858193e16e35dd68ec
psanches <psanchesnle@gmail.com>
2026-09-20 10:20:33 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,7 @@
 
+
 export const SUPABASE_URL =
   "https://hjdrnxqvmpwfztlrqknp.supabase.co";
 
 export const SUPABASE_PUBLISHABLE_KEY =
-  sb_publishable_qfhZHhgXfRq7Z3np65rF2w_WMqFWtfv;
+  "sb_publishable_qfhZHhgXfRq7Z3np65rF2w_WMqFWtfv";
