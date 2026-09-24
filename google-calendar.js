196a73c833d90804249b57558036a3a37f670b40
psanches <psanchesnle@gmail.com>
2026-09-24 19:06:38 -0300

    Update google-calendar.js

--- google-calendar.js
+++ google-calendar.js
@@ -3,7 +3,7 @@ const GOOGLE_CALENDAR_SCOPE =
   "https://www.googleapis.com/auth/calendar.events";
 
 const GOOGLE_REDIRECT_URI =
-  "https://round-lab-f54f.psanchesnle.workers.dev/auth/google/callback";
+ "https://round-lab-f54f.psanchesnle.workers.dev/api/auth/google/callback";
 
 function googleCalendarConfigured(env) {
   return Boolean(
